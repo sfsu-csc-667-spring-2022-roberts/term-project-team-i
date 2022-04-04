ecdfa3d508aa4fea5409dd683e21c72a19e591e9
Jacob Cross <jcross1@mail.sfsu.edu>
2022-04-04 11:33:37 -0700

    add readme, add static files serving

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testsRouter = require('./routes/tests')
+var testsRouter = require('./routes/tests');
+// var createGameRouter = require('./routes/createGame');
+// var joinGameRouter = require('./routes/joinGame');
+// var loginRouter = require('./routes/login');
+// var signupRouter = require('./routes/signup');
+// var landingRouter = require('./routes/landing');
+// var gamePageRouter = require('./routes/game');
+const { sign } = require("crypto");
 
 
 
@@ -27,9 +34,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+//static file serving
+app.get('/landing', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'landing.html'));
+});
+
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'login.html'));
+});
+
+app.get('/signup', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'signup.html'));
+});
+
+app.get('/joinGame', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'joinGame.html'));
+});
+
+app.get('/createGame', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'createGame.html'));
+});
+
+app.get('/game', function(req, res) {
+  res.sendFile(path.join(__dirname, './views', 'game.html'));
+});
+
+
+
+// router system for serving requests
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tests', testsRouter)
+app.use('/tests', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

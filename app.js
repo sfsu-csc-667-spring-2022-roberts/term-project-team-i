b6f0dd625476e85aeae2e7c88d8c08255e62e9c6
Anmol Burmy <anmolburmy145@gmail.com>
2022-04-04 14:42:39 -0700

    added static pages

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-
-if(process.env.NODE_ENV === 'development') {
-  require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+    require("dotenv").config();
 }
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var hbs = require("express-handlebars");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 //var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testsRouter = require('./routes/tests');
+var usersRouter = require("./routes/users");
+var testsRouter = require("./routes/tests");
 // var createGameRouter = require('./routes/createGame');
 // var joinGameRouter = require('./routes/joinGame');
 // var loginRouter = require('./routes/login');
@@ -20,67 +20,72 @@ var testsRouter = require('./routes/tests');
 // var gamePageRouter = require('./routes/game');
 const { sign } = require("crypto");
 
-
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+    "hbs",
+    hbs.engine({
+        layoutsDir: path.join(__dirname, "views/layouts"),
+        partialsDir: path.join(__dirname, "views/partials"),
+        extname: ".hbs",
+        defaultLayout: "app",
+        helpers: {
+            emptyObject: (obj) => {
+                return !(obj.constructor === Object && Object.keys(obj).length == 0);
+            },
+        },
+    })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 //static file serving
-app.get('/landing', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'landing.html'));
+app.get("/", function (req, res) {
+    res.render("landing", { title: "Welcome to UNO!" });
 });
 
-app.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'login.html'));
+app.get("/login", function (req, res) {
+    res.render("login", { title: "Welcome Back!" });
 });
 
-app.get('/signup', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'signup.html'));
+app.get("/signup", function (req, res) {
+    res.render("signup", { title: "Join Us!" });
 });
 
-app.get('/joinGame', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'joinGame.html'));
+app.get("/lobby", function (req, res) {
+    res.render("lobby", { title: "Join or Create a game!" });
 });
 
-app.get('/createGame', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'createGame.html'));
+app.get("/game", function (req, res) {
+    res.render("game", { title: "Have Fun!" });
 });
-
-app.get('/game', function(req, res) {
-  res.sendFile(path.join(__dirname, './views', 'game.html'));
-});
-
-
 
 // router system for serving requests
 //app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tests', testsRouter);
+app.use("/users", usersRouter);
+app.use("/tests", testsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;

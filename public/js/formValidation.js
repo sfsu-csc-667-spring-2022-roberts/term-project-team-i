238307b065539ea97636c87ccfe718d9e9d43b74
Anmol Burmy <anmolburmy145@gmail.com>
2022-04-24 15:12:51 -0700

    UI changes

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -8,10 +8,10 @@ function checkUnameLen() {
     let uname = document.getElementById("uname").value;
 
     if (uname.length < 3) {
-        document.getElementById("uname").style.borderColor = "red";
+        document.getElementById("uname").style.borderColor = "white";
         document.getElementById("uname_error").style.display = "block";
         document.getElementById("uname_error").innerHTML =
-            "<span style='color: red;'>&#10006 Username must be at least 3 alphanumeric characters. </span>";
+            "<span style='color: white;'>&#10006 Username must be at least 3 alphanumeric characters. </span>";
         return false;
     }
     document.getElementById("uname").style.borderColor = "green";
@@ -25,10 +25,10 @@ function checkUnameChar() {
     let uname_char = /^[a-zA-Z]+/g;
 
     if (!uname.match(uname_char)) {
-        document.getElementById("uname").style.borderColor = "red";
+        document.getElementById("uname").style.borderColor = "white";
         document.getElementById("uname_char_error").style.display = "block";
         document.getElementById("uname_char_error").innerHTML =
-            "<span style='color: red;'>&#10006 Username must start with a character (a-z or A-Z). </span>";
+            "<span style='color: white;'>&#10006 Username must start with a character (a-z or A-Z). </span>";
         return false;
     }
     document.getElementById("uname").style.borderColor = "green";
@@ -50,10 +50,10 @@ function checkPwdLen() {
     let pwd = document.getElementById("pwd").value;
 
     if (pwd.length < 8) {
-        document.getElementById("pwd").style.borderColor = "red";
+        document.getElementById("pwd").style.borderColor = "white";
         document.getElementById("pwd_error").style.display = "block";
         document.getElementById("pwd_error").innerHTML =
-            "<span style='color: red;'>&#10006 Password must be 8 characters long.</span>";
+            "<span style='color: white;'>&#10006 Password must be 8 characters long.</span>";
         return false;
     }
     document.getElementById("pwd").style.borderColor = "green";
@@ -67,10 +67,10 @@ function checkPwdChar() {
     let pwd_char = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[(/*-+!@#$^&*)])/g;
 
     if (!pwd.match(pwd_char)) {
-        document.getElementById("pwd").style.borderColor = "red";
+        document.getElementById("pwd").style.borderColor = "white";
         document.getElementById("pwd_char_error").style.display = "block";
         document.getElementById("pwd_char_error").innerHTML =
-            "<span style='color: red;'>&#10006 Password must contain at least 1 upper case letter and 1 number and 1 of the following special characters ( / * - + ! @ # $ ^ & * ).</span>";
+            "<span style='color: white;'>&#10006 Password must contain at least 1 upper case letter and 1 number and 1 of the following special characters ( / * - + ! @ # $ ^ & * ).</span>";
         return false;
     }
     document.getElementById("pwd").style.borderColor = "green";
@@ -93,10 +93,10 @@ function checkcPwdMatch() {
     let c_pwd = document.getElementById("c_pwd").value;
 
     if (pwd !== c_pwd || c_pwd == "") {
-        document.getElementById("c_pwd").style.borderColor = "red";
+        document.getElementById("c_pwd").style.borderColor = "white";
         document.getElementById("c_pwd_error").style.display = "block";
         document.getElementById("c_pwd_error").innerHTML =
-            "<span style='color: red;'>&#10006 Passwords must not be empty and must be identical.</span>";
+            "<span style='color: white;'>&#10006 Passwords must not be empty and must be identical.</span>";
         return false;
     }
     document.getElementById("c_pwd").style.borderColor = "green";
